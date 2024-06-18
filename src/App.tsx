796962a2a28b7edb34d452c644aae7c4bb54ebce
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-06-18 16:33:45 +0200

    FIX: delete functions to create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { accessToLocalStorage } from './helpers/accessToLocalStorage';
 import { lightTheme, localStorageGetAction, localStorageThemeKey } from './helpers/constants';
 import { themeDark, themeLight } from './theme/theme';
 import { ThemeContext, ThemeProvider } from 'styled-components';
-
-const router = createBrowserRouter(createRoutesFromElements(
-  <>
-
-  </>
-));
 
 function App() {
   const theme = accessToLocalStorage({ key: localStorageThemeKey, action: localStorageGetAction }) || lightTheme;
